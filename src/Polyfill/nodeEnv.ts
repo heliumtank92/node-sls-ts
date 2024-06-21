7be1ac82f864be80bdf63e9ba384e9016a6f496d
Mehul Agarwal <agarwalmehul92@gmail.com>
2024-06-22 02:05:02 +0530

    Add try-catch for nodeEnv polyfill

--- src/Polyfill/nodeEnv.ts
+++ src/Polyfill/nodeEnv.ts
@@ -3,11 +3,15 @@ import path from 'path'
 
 // Set Package Name and Version in case these are filtered out for any reason
 if (!process.env.npm_package_name) {
-  const packageJsonPath = path.resolve(__dirname, '../../package.json')
-  const packageJsonFile = fs.readFileSync(packageJsonPath).toString()
-  const packageJson = JSON.parse(packageJsonFile)
+  try {
+    const packageJsonPath = path.resolve(__dirname, '../../package.json')
+    const packageJsonFile = fs.readFileSync(packageJsonPath).toString()
+    const packageJson = JSON.parse(packageJsonFile)
 
-  const { name, version } = packageJson
-  process.env.npm_package_name = name
-  process.env.npm_package_version = version
+    const { name, version } = packageJson
+    process.env.npm_package_name = name
+    process.env.npm_package_version = version
+  } catch (error) {
+    console.error('Error polyfilling nodeEnv:', error)
+  }
 }
